59574ebbd58c88e152f457b4d16f28640a1c6980
Michael Duran <michaeldmedina05@gmail.com>
2025-06-23 18:26:23 +0000

    generador dominio terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let ext = ['.com', '.net', '.us', '.io'];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let b = 0; b < noun.length; b++) {
+        console.log(pronoun[i] + adj[a] + noun[b]);
+      }
+    }
+  }
+
+  pronoun.map((pronoun) => {
+    adj.map((adj) => {
+      noun.map((noun) => {
+        ext.map((ext) => {
+          console.log(pronoun + adj + noun + ext);
+        }
+        )
+      }
+      )
+    }
+    )
+  }
+  )
 };
